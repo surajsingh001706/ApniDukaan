bb63517fa86e3a7677e3093802be3e1f49647e14
surajsingh001706 <surajsingh001706@gmail.com>
2025-07-21 19:23:14 +0530

    Fix: Corrected authMiddleware import path in userRoutes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authmiddleware");
 
 // @route   POST /api/users/register
 // @desc    Register new user
